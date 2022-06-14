25876d406e7e7848e84dd73f07d070c85d0c6b35
Weyert de Boer <weyert.deboer@tapico.io>
2022-06-14 12:33:04 +0100

    test: add simple test case

--- src/__tests__/provider.test.ts
+++ src/__tests__/provider.test.ts
@@ -1,5 +1,28 @@
+import { Client, OpenFeature } from '@openfeature/nodejs-sdk'
+import { PostHogProvider } from '../provider'
+
+jest.mock('posthog-node')
+
+const provider = new PostHogProvider({
+  apiKey: 'api-key',
+  personalApiKey: 'personal-api-key',
+})
+
 describe('PostHogProvider', () => {
-  test('test', () => {
-    expect(true).toBe(true)
+  beforeAll(() => {
+    OpenFeature.setProvider(provider)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  let client: Client
+  test('should be able to get PostHog client', () => {
+    client = OpenFeature.getClient('posthog', '1.0.0')
+    expect(client).toBeDefined()
+    expect(client.metadata.name).toBe('posthog')
+    expect(client.metadata.version).toBe('1.0.0')
+    expect(OpenFeature.providerMetadata.name).toBe('PostHog Provider')
   })
 })
